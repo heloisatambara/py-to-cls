1a31073723b1102a063d02249afb277b44e76843
Heloísa Tambara <heloisatambara@usp.br>
2023-07-25 12:14:13 -0300

    creates method and properties and add it to the class created

--- example/dog.py
+++ example/dog.py
@@ -15,12 +15,13 @@ class Dog():
         name =  str(self.name)
         return name+" is happier now!"
     
-def get_property(myClass):
+def get_properties(myClass):
     propertyList = [attribute
                     for attribute in dir(myClass)
                     if not callable(getattr(myClass, attribute))
                     and not attribute.startswith("__")]
     return propertyList
+
 
 def get_functions(myClass,builtin=True):
     functionsList = [attribute
@@ -31,11 +32,20 @@ def get_functions(myClass,builtin=True):
                          for function in functionsList
                          if not function.startswith("__")]
     return functionsList
+
     
 def get_dataframe(myClass):
     import pandas as pd
     dataFrame = pd.DataFrame(myClass.__dict__)
     return dataFrame
+
+
+def get_implementation(function):
+    import inspect as i
+    implementation = i.getsource(function)
+    implementation = implementation.split(":")[1]
+    return implementation
+
 
 def send_iris(myClass, schema = ""):
     worked = True
@@ -48,11 +58,50 @@ def send_iris(myClass, schema = ""):
         worked = False
         print(error)
     
-    return worked    
-    
+    return worked
 
+
+def send_iris2(myClass, schema = ""):
+    worked = True
+    try:
+        # connect to the instance
+        import iris
+        connectionString, user, password = "localhost:1972/SAMPLE", "_system", "sys"
+        connection = iris.connect(connectionString, user, password)
+        irispy = iris.createIRIS(connection)
+        
+        # create the class
+        className = schema+"."+myClass.__class__.__name__
+        newClass = irispy.classMethodObject("%Dictionary.ClassDefinition", "%New", className)
+        newClass.set("Super", "%Persistent")
+        
+        # add its properties
+        propertyList = get_properties(myClass)
+        for property in propertyList:
+            newProperty = irispy.classMethodObject("%Dictionary.PropertyDefinition", "%New", className+":"+property)
+            newProperty.set("Type", "%String")
+            newClass.get("Properties").invoke("Insert", newProperty)
+    
+        # add its methods
+        methodList = get_functions(myClass)
+        for method in methodList:
+            newMethod = irispy.classMethodObject("%Dictionary.MethodDefinition", "%New", className+":"+method)
+            newMethod.set("Language", "Python")
+            newMethod.get("Implementation").invoke("Write", get_implementation(getattr(myClass, method)))
+            newClass.get("Methods").invoke("Insert", newMethod)
+        
+        # saves class
+        newClass.invoke("%Save")
+        
+    except Exception as error:
+        worked = False
+        print(error)
+        
+    return worked
 
 # the definition should set default values for the __init__ method.
+# if schema is not specified, the default will be User
 
+# TODO: method / classmethod if self is an argument
 if __name__=="__main__":
-    print(send_iris(Dog(), "test"))
+    print(send_iris2(Dog(), "pythonclass"))
